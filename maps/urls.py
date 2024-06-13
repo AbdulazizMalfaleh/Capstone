0f22abfe5858e181bed374aa7267ffb104716b78
Rayan Alshehri <116816027+ryyaon14@users.noreply.github.com>
2024-06-13 10:29:05 +0300

    Delete maps directory

--- maps/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('members/', views.members, name='members'),
-    path('process_form/', views.process_form, name='process_form')
-]
